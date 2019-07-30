2b23d5720795d5d30038933c975254b3d7954235
sekiya9311 <sekiya9311@gmail.com>
2019-07-30 21:47:37 +0900

    Initial Commit

---
+++ ProgrammingContestTemplateByDotNetCore/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingContestTemplateByDotNetCore
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
